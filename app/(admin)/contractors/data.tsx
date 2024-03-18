17730583e85fa936446e938a8ee5bd2afe703841
web-tranquility <sparkywebtranquility@gmail.com>
2024-03-18 18:40:08 +0100

    resolved data issue

--- app/(admin)/contractors/data.tsx
+++ app/(admin)/contractors/data.tsx
@@ -3,53 +3,54 @@ import { ReportType } from "./columns";
 export const data: Array<ReportType> = [
   {
     id: "1",
-    "projectName": "High Cost Renovation",
-    startDate: "6 July, 2023",
-    endDate: "6 July, 2023",
-    status: "0",
-    totalWorker: "2",
-    worker: "2",
-    materials: "0"
+    contractorName: "string",
+    address: "string",
+    officePhone: "string",
+    contactPerson: "string",
+    contactPhone: "string",
+    addedOn: "string",
+    actions: "string"
   },
   {
     id: "1",
-    "projectName": "High Cost Renovation",
-    startDate: "6 July, 2023",
-    endDate: "6 July, 2023",
-    status: "0",
-    totalWorker: "2",
-    worker: "2",
-    materials: "0"
+    contractorName: "string",
+    address: "string",
+    officePhone: "string",
+    contactPerson: "string",
+    contactPhone: "string",
+    addedOn: "string",
+    actions: "string"
   },
   {
     id: "1",
-    "projectName": "High Cost Renovation",
-    startDate: "6 July, 2023",
-    endDate: "6 July, 2023",
-    status: "0",
-    totalWorker: "2",
-    worker: "2",
-    materials: "0"
+    contractorName: "string",
+    address: "string",
+    officePhone: "string",
+    contactPerson: "string",
+    contactPhone: "string",
+    addedOn: "string",
+    actions: "string"
   },
   {
     id: "1",
-    "projectName": "High Cost Renovation",
-    startDate: "6 July, 2023",
-    endDate: "6 July, 2023",
-    status: "0",
-    totalWorker: "2",
-    worker: "2",
-    materials: "0"
+    contractorName: "string",
+    address: "string",
+    officePhone: "string",
+    contactPerson: "string",
+    contactPhone: "string",
+    addedOn: "string",
+    actions: "string"
   },
   {
     id: "1",
-    "projectName": "High Cost Renovation",
-    startDate: "6 July, 2023",
-    endDate: "6 July, 2023",
-    status: "0",
-    totalWorker: "2",
-    worker: "2",
-    materials: "0"
+    contractorName: "string",
+    address: "string",
+    officePhone: "string",
+    contactPerson: "string",
+    contactPhone: "string",
+    addedOn: "string",
+    actions: "string"
   },
+  
 
 ];
