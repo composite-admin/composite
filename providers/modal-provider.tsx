a54c722305ecb782252fc1e50bf96ed45f90fb21
web-tranquility <sparkywebtranquility@gmail.com>
2024-03-22 05:51:27 +0100

    cleared conflicts

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -29,7 +29,6 @@ export const ModalProvider = () => {
       <DetailsModal/>
       <AddAndEditBreakdownModal/>
       <ProjectSuccessModal />
-      <DetailsModal />
       <AddProjectModal />
       <SuccesModal/>
     </>
