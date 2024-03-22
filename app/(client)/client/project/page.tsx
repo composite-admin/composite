96cc646efb84d3fa1587e0fd30dec1a5698c37cd
bakemonostan <ehizomokhomion@gmail.com>
2024-03-22 14:17:37 +0100

    check build error

--- app/(client)/client/project/page.tsx
+++ app/(client)/client/project/page.tsx
@@ -1,13 +1,10 @@
-"use client";
 import { columns } from "@/app/(admin)/project/columns";
 import { data } from "@/app/(admin)/project/data";
 import { DataTable } from "@/components/shared/DataTable";
 import PageHead from "@/components/ui/pageHead";
-import { useAddProjectModal } from "@/store/inventory/UseInventoryModal";
 
 
 export default function ClientProjectPage() {
-  const onOpen = useAddProjectModal(state => state.onOpen)
   return (
     <>
       <PageHead headText="Project" subText="View all your projects here"/>
