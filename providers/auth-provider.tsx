a8fe40b8e50e360fe97602f360c4d247e47359ee
bakemonostan <ehizomokhomion@gmail.com>
2024-03-29 15:36:08 +0100

    add temp staff and client redirects

--- providers/auth-provider.tsx
+++ providers/auth-provider.tsx
@@ -1,25 +1,52 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import useAuthStore, { userStore } from "@/store/auth/AuthStore";
+import { useRouter, usePathname } from "next/navigation";
+
+import { userStore } from "@/store/auth/AuthStore";
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const { token } = userStore();
+  const { token, userType } = userStore();
   const router = useRouter();
+  const pathname = usePathname();
   useEffect(() => {
     const handleRedirect = () => {
       if (!token) {
         router.push("/login");
       } else {
-        router.push("/");
+        if (userType === "admin") {
+          // Admin can access any route
+          router.push("/");
+        } else if (userType === "client") {
+          const currentPath = pathname;
+          if (currentPath.startsWith("/client")) {
+            // Client is accessing a valid route
+            router.push(currentPath);
+          } else {
+            // Client is trying to access an invalid route, redirect to /client
+            router.push("/client/dashboard");
+          }
+        } else if (userType === "staff") {
+          const currentPath = pathname;
+          if (currentPath.startsWith("/staff/dashboard")) {
+            // Staff is accessing a valid route
+            router.push(currentPath);
+          } else {
+            // Staff is trying to access an invalid route, redirect to /staff
+            router.push("/staff");
+          }
+        }
       }
     };
 
     handleRedirect();
-  }, [token, router]);
+  }, [token, userType, router, pathname]);
 
   return <>{children}</>;
 };
 
 export default AuthProvider;
+
+
+// "email": "bola@composite.com",
+//     "password": "composite"
