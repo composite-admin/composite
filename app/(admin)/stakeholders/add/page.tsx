9fa2cd692a7ff9567b4e7a10f67528e9788998b3
web-tranquility <162973978+web-tranquility@users.noreply.github.com>
2024-04-11 08:53:22 +0000

    code update

--- app/(admin)/stakeholders/add/page.tsx
+++ app/(admin)/stakeholders/add/page.tsx
@@ -16,100 +16,98 @@ const AddStakeholder = () => {
     const onSubmit = (data: any) => {
         // Pass the form data to your submitForm action
         console.log(data)
-        
+
         createStakeholder(data);
         onOpen()
         reset()
         return;
-      };
+    };
     return (
         <>
             <GoBack />
             <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="w-[80%] mx-auto my-10 rounded-lg border border-outline bg-white p-[29px]">
-                <div className="flex gap-2 flex-col border-b border-b-gray-200 py-3">
+                <div className="w-[80%] mx-auto my-10 rounded-lg border border-outline bg-white p-[29px]">
+                    <div className="flex gap-2 flex-col border-b border-b-gray-200 py-3">
 
-                    <h2 className="text-[#101928] font-[600] text-[22px]">Add Stakeholder</h2>
-                    <p>Update Stakeholder Details here.</p>
-                </div>
-
-                <div className="grid grid-cols-2 gap-5 my-5 edit">
-                    <div className="flex flex-col">
-                        <p className="value">
-                            Stakeholder Name
-                        </p>
-
-                        <input type="text" {...register('stakeholder_name', { required: true })}/>
+                        <h2 className="text-[#101928] font-[600] text-[22px]">Add Stakeholder</h2>
+                        <p>Update Stakeholder Details here.</p>
                     </div>
 
-                    <div className="flex flex-col">
-                        <p className="value">
-                            Address
-                        </p>
+                    <div className="grid grid-cols-2 gap-5 my-5 edit">
+                        <div className="flex flex-col">
+                            <p className="value">
+                                Stakeholder Name
+                            </p>
 
-                        <input type="text" {...register('stakeholder_address', { required: true })}/>
-                    </div>
-
-                    <div className="flex flex-col">
-                        <p className="value">
-                            Stakeholder Phone
-                        </p>
-
-                        <input type="number" {...register('stakeholder_ofc_phone', { required: true })}/>
-                    </div>
-
-                    <div className="flex flex-col">
-                        <p className="value">
-                            Contact Person
-                        </p>
-
-                        <input type="text" {...register('contact_person', { required: true })}/>
-                    </div>
-
-                    <div className="flex flex-col">
-                        <p className="value">
-                            Contact Home Phone
-                        </p>
-
-                        <input type="number" {...register('contact_home_phone', { required: true })}/>
-                    </div>
-
-                    <div className="flex flex-col">
-                        <p className="value">
-                            Contact Mobile
-                        </p>
-
-                        <input type="number" {...register('contact_mobile', { required: true })}/>
-                    </div>
-
-                    <div className="flex flex-col">
-                        <p className="value">
-                            Non Government Agency
-                        </p>
-
-                        <input type="text" {...register('non_government_agencies', { required: true })}/>
-                    </div>
-
-                    <div className="flex flex-col">
-                        <p className="value">
-                            Government Agency
-                        </p>
-
-                        <input type="text" {...register('government_agencies', { required: true })}/>
-                    </div>
-
-                    <div className="flex flex-col col-span-2">
-                        <div className="value">
-                            Comment
+                            <input type="text" {...register('stakeholder_name', { required: true })} />
                         </div>
 
-                        <textarea {...register('comment', { required: true })}/>
-                    </div>
+                        <div className="flex flex-col">
+                            <p className="value">
+                                Address
+                            </p>
 
-                    <button className="bg-[#EBEBEB] text-textColor rounded-md" onClick={()=> router.back()} >Cancel</button>
-                    <button className="bg-primaryLight text-white  p-3 rounded-md" type="submit">Submit</button>
+                            <input type="text" {...register('stakeholder_address', { required: true })} />
+                        </div>
+
+                        <div className="flex flex-col">
+                            <p className="value">
+                                Stakeholder Phone
+                            </p>
+
+                            <input type="number" {...register('stakeholder_ofc_phone', { required: true })} />
+                        </div>
+
+                        <div className="flex flex-col">
+                            <p className="value">
+                                Contact Person
+                            </p>
+
+                            <input type="text" {...register('contact_person', { required: true })} />
+                        </div>
+                        <div className="flex flex-col">
+                            <p className="value">
+                                Contact Home Phone
+                            </p>
+                            <input type="number" {...register('contact_home_phone', { required: true })} />
+                        </div>
+                        <div className="flex flex-col">
+                            <p className="value">
+                                Contact Mobile
+                            </p>
+
+                            <input type="number" {...register('contact_mobile', { required: true })} />
+                        </div>
+
+                        <div className="flex flex-col">
+                            <p className="value">
+                                Non Government Agency
+                            </p>
+                            <select {...register('non_government_agencies', { required: true })} >
+                                <option value="a">A</option>
+                                <option value="b">B</option>
+                            </select>
+                        </div>
+
+                        <div className="flex flex-col">
+                            <p className="value">
+                                Government Agency
+                            </p>
+                            <select {...register('government_agencies', { required: true })} >
+                                <option value="a">A</option>
+                                <option value="b">B</option>
+                            </select>
+                        </div>
+                        <div className="flex flex-col col-span-2">
+                            <div className="value">
+                                Comment
+                            </div>
+                            <textarea {...register('comment', { required: true })} />
+                        </div>
+                        <button className="bg-[#EBEBEB] text-textColor rounded-md" onClick={() => router.back()} >Cancel</button>
+                        <button className="bg-primaryLight text-white  p-3 rounded-md" type="submit">Submit</button>
+                    </div>
                 </div>
-            </div>
             </form>
         </>
     )
