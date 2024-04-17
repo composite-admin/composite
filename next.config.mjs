608cb6f8cbd579c18dc36f6c1cd226d771cd2911
bakemonostan <ehizomokhomion@gmail.com>
2024-04-17 08:37:11 +0100

    add hostname to next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,16 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "variety.com",
-      },
-    ],
-  },
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "composite-portal.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "variety.com",
       },
     ],
   },
