32c7803372a6087f0edf11bd811a63b993d17b01
bakemonostan <ehizomokhomion@gmail.com>
2024-06-02 18:56:15 +0100

    update

--- app/(admin)/manage-staff/team-manager/page.tsx
+++ app/(admin)/manage-staff/team-manager/page.tsx
@@ -7,17 +7,20 @@ import { columns } from "./columns";
 export default function ManageStaffPage() {
   const { isLoading, projectTeamMemberData } = useGetAllProjectTeamMeambers();
 
-  console.log(projectTeamMemberData);
   return (
     <div className="space-y-8">
       <div>
         <PageHeaderComponent
-          title="Project Team Member"
+          title={`Project Team Member (${projectTeamMemberData?.length ?? 0})`}
           subTitle="View all staff here"
           className="py-8"
         />
       </div>
-      <DataTable columns={columns} data={projectTeamMemberData || []} />
+      <DataTable
+        columns={columns}
+        data={projectTeamMemberData || []}
+        isLoading={isLoading}
+      />
     </div>
   );
 }
