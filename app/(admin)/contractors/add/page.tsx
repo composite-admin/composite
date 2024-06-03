039c8e006159844b6c05e78a88787ff6c6857a5a
bakemonostan <ehizomokhomion@gmail.com>
2024-06-03 18:22:03 +0100

    update contractors

--- app/(admin)/contractors/add/page.tsx
+++ app/(admin)/contractors/add/page.tsx
@@ -148,6 +148,7 @@ const AddContractor = () => {
             <button
               className="bg-[#EBEBEB] text-textColor rounded-md"
               onClick={() => router.back()}
+              type="button"
             >
               Cancel
             </button>
