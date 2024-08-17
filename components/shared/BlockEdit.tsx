ac9c8c1d522ff093b1228e30156d471c9977896d
ehiz <ehizomokhomion@gmail.com>
2024-08-17 13:35:17 +0100

    push

--- components/shared/BlockEdit.tsx
+++ components/shared/BlockEdit.tsx
@@ -5,7 +5,9 @@ export function BlockEdiComponent() {
     <>
       <GoBack />
       <div className="flex justify-center items-center flex-col h-screen">
-        <p className="text-2xl font-bold">You do not have permission to Edit</p>
+        <p className="text-2xl font-bold">
+          You do not have permission to carry out this action
+        </p>
         <em>Contact your administrator</em>
       </div>
     </>
